c79637df3f35db82411fb6be8064aa92ec9b60e8
ajiro <ajiro99@gmail.com>
2017-04-22 10:23:37 +0900

    ショッピングカート実装 コントローラー修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -27,37 +27,6 @@ class Potepan::ProductsController < ApplicationController
     @prototypes = Spree::Prototype.all
   end
 
-  def cart
-
-  end
-
-    def populate
-      order    = current_order(create_order_if_necessary: true)
-      variant  = Spree::Variant.find(params[:variant_id])
-      quantity = params[:quantity].to_i
-
-      # 2,147,483,647 is crazy. See issue https://github.com/spree/spree/issues/2695.
-      if quantity.between?(1, 2_147_483_647)
-        begin
-          order.contents.add(variant, quantity)
-        rescue ActiveRecord::RecordInvalid => e
-          error = e.record.errors.full_messages.join(", ")
-        end
-      else
-        error = Spree.t(:please_enter_reasonable_quantity)
-      end
-
-      if error
-        flash[:error] = error
-        redirect_back_or_default(spree.root_path)
-      else
-        # respond_with(order) do |format|
-        #   format.html { redirect_to cart_potepan_products_path }
-        # end
-        redirect_to cart_potepan_products_path
-      end
-    end
-
 	private
     def load_product
       if try_spree_current_user.try(:has_spree_role?, "admin")
